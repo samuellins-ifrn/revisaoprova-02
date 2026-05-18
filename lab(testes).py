d15255401545f4b658b629f49e54867aa80eed7d
samuellins-ifrn <samuellins@ufrn.edu.br>
2026-05-18 20:46:18 -0300

    commit2

---
+++ lab(testes).py
@@ -0,0 +1,12 @@
+# crescente
+n = 0
+while n < 100:
+    n += 1
+    print(n)
+
+# decrescente
+n = 100
+while n > 1:
+    n -= 1
+    print(n)
+
---
+++ revisaopy.py
@@ -0,0 +1,7 @@
+# 1. Escreva um algoritmo que imprima na tela do usuário os números inteiros de 1 até 100 (em ordem crescente).
+for n in range(1,100 + 1):
+    print(n)
+
+# decrescente
+for n in range(1,100, -1):
+    print(n)
